7b824bd3e2a820f65526f94df2da16af8f7b0f17
Luci-Lawless <lu.gmmm@gmail.com>
2018-07-13 16:10:34 +0200

    Getting data from Marvel API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { MY_KEY } from './config';
+import { MY_HASH } from './config'
 import Form from './Components/Form';
 
 class App extends Component {
+  state = {
+    names: []
+  }
 
-  getCharacter = (e) => {
+  getCharacter = async (e) => {
     e.preventDefault();
     const characterName = e.target.elements.characterName.value;
-    console.log(characterName);
+    const api_call = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${MY_KEY}&hash=${MY_HASH}`);
+    const data = await api_call.json();
+    console.log(data);
+
+
   }
   render() {
     return (
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,6 +6,9 @@ class Form extends React.Component {
       <form onSubmit={this.props.getCharacter}>
         <input className="form-input" type="text" name="characterName" />
         <button>Search</button>
+
+          <div>{this.getCharacter}</div>
+
       </form>
     );
   }
